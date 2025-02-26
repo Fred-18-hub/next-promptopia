24a42135ba9e0662d22b997752a9abd45b27af98
Fredrick Appiah <apfred18@gmail.com>
2025-02-26 13:25:24 +0000

    create prompt completed

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,5 +1,56 @@
+"use client";
+
+import { useCallback, useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import Form from "@components/Form";
+
 const CreatePrompt = () => {
-  return <div>CreatePrompt</div>;
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
+
+  const createPrompt = async (e) => {
+    e.preventDefault();
+
+    try {
+      setSubmitting(true);
+
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          userId: session?.user.id,
+          prompt: post.prompt,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <>
+      <Form
+        type="Create"
+        post={post}
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={createPrompt}
+      />
+    </>
+  );
 };
 
 export default CreatePrompt;
