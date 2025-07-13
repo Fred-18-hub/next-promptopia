b5a947f92ffc56e57873560a5f3d89ca03169478
Fredrick Appiah <apfred18@gmail.com>
2025-07-13 13:12:14 +0000

    refactor: restructure UpdatePrompt component and implement loading state

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,72 +1,13 @@
-"use client";
+import React, { Suspense } from "react";
+import UpdatePrompt from "@components/UpdatePrompt";
+import Loading from "@components/Loading";
 
-import { useEffect, useState } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
-import Form from "@components/Form";
-
-const UpdatePrompt = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-
-  const promptId = searchParams.get("id");
-
-  const [submitting, setSubmitting] = useState(false);
-  const [post, setPost] = useState({
-    prompt: "",
-    tag: "",
-  });
-
-  useEffect(() => {
-    const fetchPostDetails = async () => {
-      const response = await fetch(`/api/prompt/${promptId}`);
-      const data = await response.json();
-
-      setPost({
-        prompt: data.prompt,
-        tag: data.tag,
-      });
-    };
-
-    promptId && fetchPostDetails();
-  }, [promptId]);
-
-  const editPrompt = async (e) => {
-    e.preventDefault();
-
-    if (!promptId) return alert("Prompt ID not found");
-
-    try {
-      setSubmitting(true);
-
-      const response = await fetch(`/api/prompt/${promptId}`, {
-        method: "PATCH",
-        body: JSON.stringify({
-          prompt: post.prompt,
-          tag: post.tag,
-        }),
-      });
-
-      if (response.ok) {
-        router.push("/profile");
-      }
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setSubmitting(false);
-    }
-  };
-
+const Page = () => {
   return (
-    <>
-      <Form
-        type="Edit"
-        post={post}
-        setPost={setPost}
-        submitting={submitting}
-        handleSubmit={editPrompt}
-      />
-    </>
+    <Suspense fallback={<Loading />}>
+      <UpdatePrompt />
+    </Suspense>
   );
 };
 
-export default UpdatePrompt;
+export default Page;
